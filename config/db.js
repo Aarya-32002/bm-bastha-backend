0b8416b433125ddaa6f40f50a00bee8bc8408607
unknown <bmbastha@gmail.com>
2026-05-13 21:45:59 +0530

    fixed database connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,25 @@
 const mysql = require('mysql2');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env')
+});
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'shinkansen.proxy.rlwy.net',
+
+  port: process.env.DB_PORT || 10543,
+
   user: process.env.DB_USER || 'root',
+
   password: process.env.DB_PASSWORD || 'EKZxgpIdYqdYXvNgISMmXevSJZajJkIz',
+
   database: process.env.DB_NAME || 'railway',
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -20,7 +33,9 @@ pool.getConnection((err, connection) => {
     console.error('❌ Database connection failed:', err.message);
     return;
   }
+
   console.log('✅ MySQL connected successfully');
+
   connection.release();
 });
 
